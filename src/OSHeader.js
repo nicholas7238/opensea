19c976c2edfb8b719152e2ca6933e65895c4d3b3
nicholct <67173781+nicholct@users.noreply.github.com>
2022-01-08 20:34:54 -0800

    commit2

--- src/OSHeader.js
+++ src/OSHeader.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import opensea from './opensea.svg';
 import './OSHeader.css';
+import SearchIcon from '@mui/icons-material/Search';
+import WalletIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ProfileIcon from '@mui/icons-material/AccountCircleOutlined';
+import MenuIcon from '@mui/icons-material/Menu'
 
 export default function OSHeader() {
     return (
         <div className='OSHeader'>
-            <div className='OSHeader-div'>
+            <div className='OSHeader-div Logo'>
                 <img className='OSHeader-img' src={opensea} alt="logo" />
                 <b className='OSHeader-b'> OpenSea </b>
-                <input className='OSHeader-input' type='text' placeholder='Search OpenSea'></input>
+                
+                
             </div>
-            <div className='OSHeader-div'>
+            <div className='OSHeader-div SearchBar'>
+                <SearchIcon className='OSHeader-search'></SearchIcon>
+                <input className='OSHeader-input' type='text' placeholder='Search items, collections, and accounts'></input>
+            </div>
+
+            <div className='OSHeader-div RightMenu'>
+            
+            <div className='OSHeader-div Menu1'>
                 <ul className='OSHeader-ul'>
                     <li>Explore</li>
                     <li>Stats</li>
                     <li>Resources</li>
                     <li>Create</li>
                 </ul>
+            </div>
+            <div className='OSHeader-div Menu2'>           
+                <ProfileIcon className='OSHeader-icon'></ProfileIcon>
+                <WalletIcon className='OSHeader-icon'></WalletIcon>
+            </div>
+            <div className='OSHeader-div Hamburger'>
+                <MenuIcon className='OSHeader-icon'></MenuIcon>
+            </div>
             
             </div>
+
         </div>
     )
 }
